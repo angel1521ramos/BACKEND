47fe34e7c5f728e81c8e1feac1bb32aa3310a832
jose <angel1521ramos@gmail.com>
2021-02-05 20:02:29 -0600

    02 - creacion de ruta de prueba

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./clases/server"));
+const usuario_1 = __importDefault(require("./routes/usuario"));
 const server = new server_1.default();
+//rutas de la aplicacion
+server.app.use('/user', usuario_1.default);
+//levantar servidor de express
 server.start(() => {
     console.log(`servidor corriendo en puerto ${server.port}`);
 });
--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 import Server from "./clases/server";
+import userRoutes from "./routes/usuario";
 
 const server =  new Server();
+
+//rutas de la aplicacion
+server.app.use('/user', userRoutes);
+
+//levantar servidor de express
 server.start(()=>{
 console.log(`servidor corriendo en puerto ${ server.port }`);
 })
